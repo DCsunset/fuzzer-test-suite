6a6ff053176aca2aad8648df2b99e2fd20e6c1c3
DCsunset <DCsunset@users.noreply.github.com>
2022-03-17 12:14:43 -0400

    fix: fix test-afl.sh for boringssl

--- project-scripts/boringssl-2016-02-12/test-afl.sh
+++ project-scripts/boringssl-2016-02-12/test-afl.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Copyright 2016 Google Inc. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
 . $(dirname $0)/../../fuzzer-test-suite/common.sh
 
-# Note: this target contains unbalanced malloc/free (malloc is called
-# in one invocation, free is called in another invocation).
-# and so libFuzzer's -detect_leaks should be disabled for better speed.
-export ASAN_OPTIONS=detect_leaks=0:quarantine_size_mb=50
-
 set -x
-rm -rf $CORPUS fuzz-*.log
+rm -rf $CORPUS
 mkdir $CORPUS
-[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -i seeds -o $CORPUS ./$EXECUTABLE_NAME_BASE -N$JOBS
+
+# Master
+[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE &> fuzzer1.log &
+for i in $(seq 2 $JOBS); do
+	# secondary
+	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE &> fuzzer$i.log &
+done
