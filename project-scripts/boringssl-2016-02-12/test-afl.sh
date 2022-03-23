e0348227259579ff5eeaad4e01c3ab88873c088b
DCsunset <DCsunset@users.noreply.github.com>
2022-03-22 20:52:41 -0400

    fix: fix boringssl script

--- project-scripts/boringssl-2016-02-12/test-afl.sh
+++ project-scripts/boringssl-2016-02-12/test-afl.sh
@@ -6,8 +6,8 @@ rm -rf $CORPUS
 mkdir $CORPUS
 
 # Master
-[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE &> fuzzer1.log &
+[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE @@ &> fuzzer1.log &
 for i in $(seq 2 $JOBS); do
 	# secondary
-	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE &> fuzzer$i.log &
+	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE @@ &> fuzzer$i.log &
 done
