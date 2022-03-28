6686e85cd81e1a2892cfaae86fbcc9c3538d51b4
DCsunset <DCsunset@users.noreply.github.com>
2022-03-27 23:07:58 -0400

    feat: add AFL scripts for most projects

---
+++ project-scripts/woff2/test-afl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+. $(dirname $0)/../../fuzzer-test-suite/common.sh
+
+set -x
+rm -rf $CORPUS
+mkdir $CORPUS
+
+# Master
+[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE @@ &> fuzzer1.log &
+for i in $(seq 2 $JOBS); do
+	# secondary
+	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE @@ &> fuzzer$i.log &
+done
