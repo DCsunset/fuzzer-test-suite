59c63526fbc144907634493290f81ca7334f7605
DCsunset <DCsunset@users.noreply.github.com>
2022-04-02 00:58:14 -0400

    fix: fix lcms project script

--- project-scripts/lcms-2017-03-21/test-afl.sh
+++ project-scripts/lcms-2017-03-21/test-afl.sh
@@ -8,11 +8,11 @@ mkdir $CORPUS
 EXECUTABLE_NAME_COVERAGE=$(basename $SCRIPT_DIR)-coverage
 seed_dirs="$CORPUS/fuzzer1/queue"
 # Master
-[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i $SCRIPT_DIR/seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE @@ &> fuzzer1.log &
+[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i $ORIGINAL_SCRIPT_DIR/seeds -o $CORPUS -M fuzzer1 ./$EXECUTABLE_NAME_BASE @@ &> fuzzer1.log &
 for i in $(seq 2 $JOBS); do
 	seed_dirs="$seed_dirs $CORPUS/fuzzer$i/queue"
 	# secondary
-	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i $SCRIPT_DIR/seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE @@ &> fuzzer$i.log &
+	[ -e $EXECUTABLE_NAME_BASE ] && $AFL_SRC/afl-fuzz -m 800 -i $ORIGINAL_SCRIPT_DIR/seeds -o $CORPUS -S fuzzer$i ./$EXECUTABLE_NAME_BASE @@ &> fuzzer$i.log &
 done
 
 sleep 10
