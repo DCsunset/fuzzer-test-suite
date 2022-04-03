6e507dbd344e6b12b1fe7e8a245c7ac3be40db07
DCsunset <DCsunset@users.noreply.github.com>
2022-04-03 12:09:03 -0400

    fix: add openssl-1.0.2d proj

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-AVAIL_PROJ=(boringssl c-ares freetype2 guetzli harfbuzz json lcms libarchive libjpeg libpng libssh libxml2 llvm-libcxxabi openssl-1.0.1f openssl-1.1.0c pcre2 proj4 re2 sqlite vorbis woff2)
+AVAIL_PROJ=(boringssl c-ares freetype2 guetzli harfbuzz json lcms libarchive libjpeg libpng libssh libxml2 llvm-libcxxabi openssl-1.0.2d pcre2 proj4 re2 sqlite vorbis woff2)
 
 BASEDIR=$(readlink -f $(dirname $0))
 PARENT_DIR="$BASEDIR/RUN_EXPERIMENT"
